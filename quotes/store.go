84afa1f725b701ec4855aa7321739915070a55dc
bfrancisco <francisco.jm@pg.com>
2026-07-09 15:51:44 +0800

    add: Store interface; in-memory and firebase implmntn separated

---
+++ quotes/store.go
@@ -0,0 +1,16 @@
+package quotes
+
+import "context"
+
+type Store interface {
+	CreateQuote(ctx context.Context, quote Quote) error
+
+	ListQuotes(ctx context.Context) ([]Quote, error)
+	GetQuoteByID(ctx context.Context, id string) (Quote, error)
+	GetQuotesByAuthor(ctx context.Context, author string) ([]Quote, error)
+	GetRandomQuote(ctx context.Context) (Quote, error)
+
+	UpdateQuote(ctx context.Context, quote Quote) error
+
+	DeleteQuote(ctx context.Context, id string) error
+}
