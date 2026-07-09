391a353cd5f0d23da1d72b8f8a2515191bc0dfd4
bfrancisco <francisco.jm@pg.com>
2026-07-09 18:05:34 +0800

    edit: isNumeric function

--- quotes/helper.go
+++ quotes/helper.go
@@ -5,3 +5,12 @@ import "strings"
 func isEmptyString(input string) bool {
 	return strings.TrimSpace(input) == ""
 }
+
+func isNumeric(input string) bool {
+	for _, char := range strings.TrimSpace(input) {
+		if char < '0' || char > '9' {
+			return false
+		}
+	}
+	return true
+}
