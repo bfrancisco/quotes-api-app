4f77d396d37c6884001947a71d35df58e7945e70
bfrancisco <francisco.jm@pg.com>
2026-07-10 18:27:57 +0800

    fix: edit create and update signatures

--- quotes/store.go
+++ quotes/store.go
@@ -3,14 +3,14 @@ package quotes
 import "context"
 
 type Store interface {
-	CreateQuote(ctx context.Context, quote Quote) error
+	CreateQuote(ctx context.Context, quote QuoteCreateInput) error
 
 	ListQuotes(ctx context.Context) ([]Quote, error)
 	GetQuoteByID(ctx context.Context, id string) (Quote, error)
 	GetQuotesByAuthor(ctx context.Context, author string) ([]Quote, error)
 	GetRandomQuote(ctx context.Context) (Quote, error)
 
-	UpdateQuote(ctx context.Context, quote Quote) error
+	UpdateQuote(ctx context.Context, quote QuoteUpdateInput) error
 
 	DeleteQuote(ctx context.Context, id string) error
 }
